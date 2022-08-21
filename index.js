b75564a4c5cda5dedf40876e67f8743636ae8791
ShaunM <seansama__@pop-os.localdomain>
2022-08-21 13:57:35 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Code your solution in this file!
+function  distanceFromHqInBlocks(blocks){
+
+    if(blocks >= 42){
+        return blocks - 42;
+    }
+    else{
+        return 42 - blocks;
+    }
+}
+
+function  distanceFromHqInFeet(blocks){
+    return (distanceFromHqInBlocks(blocks)*264)
+
+}
+
+function  distanceTravelledInFeet(start, destination){
+    if (start < destination){
+        return (destination - start)*264;
+    }
+    else{
+        return (start - destination)*264;
+    }
+
+}
+
+function calculatesFarePrice(start, destination){
+    let distanceFeet = distanceTravelledInFeet(start, destination);
+    if (distanceFeet <= 400){
+        return 0;
+    }
+    else if(distanceFeet > 400 && distanceFeet <= 2000){
+        return (distanceFeet - 400)*.02;
+    }
+    else if(distanceFeet > 2000 && distanceFeet <= 2500){
+        return 25;
+    }
+    else{
+        return `cannot travel that far`;
+    }
+    
+}
